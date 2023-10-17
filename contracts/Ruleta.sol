dd4dc6e43254ae9aaa540bdd14eb72c8883fc90b
Matias Kochman <matiaskochman@gmail.com>
2023-10-17 14:03:21 -0300

    feat: generamos un ganador

--- contracts/Ruleta.sol
+++ contracts/Ruleta.sol
@@ -1,47 +1,57 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-
-contract Ruleta {
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "hardhat/console.sol";
+contract Ruleta is Ownable {
     IERC20 private token;
-    
+
+    enum GameState { CREADO, SE_PERMITEN_APUESTAS, NO_SE_PERMITEN_APUESTAS, TERMINADO }
+
     struct Bet {
         uint256 id;
         address player;
         uint256 amount;
-        uint8 number; // Assumes that you'll validate that it's between 0 and 36 elsewhere
+        uint8 number;
     }
 
     struct Game {
         uint256 id;
+        GameState state;
         uint8 winnerNumber;
         address[] winners;
     }
+
     uint256 private currentGameId = 0;
-    Game[] private games;
+    mapping(uint256 => Game) public games;
     mapping(uint256 => uint8) public gameIdToBetIdCounterMap;
-    mapping(uint256 => Game) public gameMap;
     mapping(uint256 => mapping(uint8 => Bet)) public gameToBetMap;
+
     constructor(address _token) {
         token = IERC20(_token);
     }
 
     function createGame() public {
-        Game memory game = Game({id: currentGameId, winnerNumber:0,winners: new address[](0)});
-        game.id = currentGameId;
-        gameMap[currentGameId] = game;
+        Game memory newGame = Game({
+            id: currentGameId,
+            state: GameState.CREADO,
+            winnerNumber: 0,
+            winners: new address[](0)
+        });
+
+        games[currentGameId] = newGame;
         gameIdToBetIdCounterMap[currentGameId] = 0;
         currentGameId++;
     }
 
-    function bet(uint256 gameId, uint256 amount, uint8 number) public {
-        require(token.balanceOf(msg.sender) >= amount, "Insufficient balance for bet");
-        require(number >= 0 && number <= 36, "Invalid number. Must be between 0 and 36");
+    function betInGame(uint256 gameId, uint256 amount, uint8 number) public {
+        require(games[gameId].state == GameState.SE_PERMITEN_APUESTAS, "Las apuestas no estan permitidas para este juego en este momento");
+        require(token.balanceOf(msg.sender) >= amount, "Saldo insuficiente para apostar");
+        require(number >= 0 && number <= 36, "Numero no valido. Debe estar entre 0 y 36");
 
-        // Transfer the tokens to this contract
         token.transferFrom(msg.sender, address(this), amount);
 
-        // Store the bet
         uint8 betId = gameIdToBetIdCounterMap[gameId];
         Bet memory newBet = Bet({
             id: betId,
@@ -49,14 +59,40 @@ contract Ruleta {
             amount: amount,
             number: number
         });
-        
-        // Search for the game
+
         gameToBetMap[gameId][betId] = newBet;
-        // Game storage game = games[gameId];
         gameIdToBetIdCounterMap[gameId]++;
     }
 
-    function setWinnerNumber(uint8 _winnerNumber, uint256 _gameId) public {
-
+    function setGameState(uint256 gameId, GameState newState) public onlyOwner {
+        games[gameId].state = newState;
     }
+
+    function setWinnerNumber(uint256 gameId) public onlyOwner {
+        require(games[gameId].state == GameState.NO_SE_PERMITEN_APUESTAS, "El juego debe estar en estado 'NO_SE_PERMITEN_APUESTAS' para seleccionar un numero ganador");
+        
+        uint8 totalBets = gameIdToBetIdCounterMap[gameId];
+        
+        // Asegurarse de que hay apuestas antes de intentar seleccionar un ganador
+        require(totalBets > 0, "No hay apuestas para este juego");
+
+        // Generar un número aleatorio que se utilizará como índice para seleccionar al ganador
+        uint8 winningIndex = uint8(uint256(keccak256(abi.encodePacked(block.timestamp, block.difficulty, msg.sender))) % totalBets);
+        
+        // Utilizar el número aleatorio como índice para seleccionar al ganador
+        Bet storage winningBet = gameToBetMap[gameId][winningIndex];
+        uint8 winnerNumber = winningBet.number;
+
+        games[gameId].winnerNumber = winnerNumber;
+        games[gameId].winners.push(winningBet.player);
+        
+        // Aquí podrías también transferir tokens ganados al jugador
+        
+        games[gameId].state = GameState.TERMINADO;
+    }
+
+    function getGameWinners(uint256 gameId) public view returns(address[] memory) {
+        return games[gameId].winners;
+    }
+
 }
--- test/Ruleta.js
+++ test/Ruleta.js
@@ -50,100 +50,54 @@ describe("Ruleta", function () {
       }
 
       await ruleta.createGame();
-
+      await ruleta.setGameState(0, 1);
       const gameId = 0;
       const betAmount = 100;
       const numberPicked = 5;
       for (let index = 0; index < users.length; index++) {
-        await ruleta.connect(users[index]).bet(gameId, betAmount, numberPicked);
+        await ruleta
+          .connect(users[index])
+          .betInGame(gameId, betAmount, numberPicked);
       }
       const a = await stableTokenMock.balanceOf(ruleta.address);
-      console.log(a);
+      console.log("balance of ruleta contract: ", a);
     });
-
-    // it("Should receive and store the funds to lock", async function () {
-    //   const { lock, lockedAmount } = await loadFixture(
-    //     deployOneYearLockFixture
-    //   );
-
-    //   expect(await ethers.provider.getBalance(lock.address)).to.equal(
-    //     lockedAmount
-    //   );
-    // });
-
-    // it("Should fail if the unlockTime is not in the future", async function () {
-    //   // We don't use the fixture here because we want a different deployment
-    //   const latestTime = await time.latest();
-    //   const Lock = await ethers.getContractFactory("Lock");
-    //   await expect(Lock.deploy(latestTime, { value: 1 })).to.be.revertedWith(
-    //     "Unlock time should be in the future"
-    //   );
-    // });
   });
+  describe("Winning Scenario", function () {
+    it("Should let one player win", async function () {
+      const { ruleta, stableTokenMock, owner, users } = await loadFixture(
+        deployOneYearLockFixture
+      );
 
-  // describe("Withdrawals", function () {
-  //   describe("Validations", function () {
-  //     it("Should revert with the right error if called too soon", async function () {
-  //       const { lock } = await loadFixture(deployOneYearLockFixture);
+      // Transfer some tokens to the players and approve the contract
+      for (let index = 0; index < users.length; index++) {
+        await stableTokenMock.transfer(users[index].address, 100);
+        await stableTokenMock
+          .connect(users[index])
+          .approve(ruleta.address, 100);
+      }
 
-  //       await expect(lock.withdraw()).to.be.revertedWith(
-  //         "You can't withdraw yet"
-  //       );
-  //     });
+      // Create a new game
+      await ruleta.createGame();
+      await ruleta.setGameState(0, 1); // Assuming 1 is the state where bets are allowed
 
-  //     it("Should revert with the right error if called from another account", async function () {
-  //       const { lock, unlockTime, otherAccount } = await loadFixture(
-  //         deployOneYearLockFixture
-  //       );
+      // All players bet on different numbers
+      for (let index = 0; index < users.length; index++) {
+        await ruleta.connect(users[index]).betInGame(0, 10, index);
+      }
 
-  //       // We can increase the time in Hardhat Network
-  //       await time.increaseTo(unlockTime);
+      // Move the game to a state where the winner can be decided
+      await ruleta.setGameState(0, 2); // Assuming 2 means no more bets allowed
 
-  //       // We use lock.connect() to send a transaction from another account
-  //       await expect(lock.connect(otherAccount).withdraw()).to.be.revertedWith(
-  //         "You aren't the owner"
-  //       );
-  //     });
+      // Decide the winner. This is tricky because the winning number is random.
+      // In a real test environment, you would mock the randomness for testability.
+      await ruleta.setWinnerNumber(0);
 
-  //     it("Shouldn't fail if the unlockTime has arrived and the owner calls it", async function () {
-  //       const { lock, unlockTime } = await loadFixture(
-  //         deployOneYearLockFixture
-  //       );
+      const winners = await ruleta.getGameWinners(0);
+      console.log(winners);
+      expect(winners.length).to.equal(1);
 
-  //       // Transactions are sent using the first signer by default
-  //       await time.increaseTo(unlockTime);
-
-  //       await expect(lock.withdraw()).not.to.be.reverted;
-  //     });
-  //   });
-
-  //   describe("Events", function () {
-  //     it("Should emit an event on withdrawals", async function () {
-  //       const { lock, unlockTime, lockedAmount } = await loadFixture(
-  //         deployOneYearLockFixture
-  //       );
-
-  //       await time.increaseTo(unlockTime);
-
-  //       await expect(lock.withdraw())
-  //         .to.emit(lock, "Withdrawal")
-  //         .withArgs(lockedAmount, anyValue); // We accept any value as `when` arg
-  //     });
-  //   });
-
-  //   describe("Transfers", function () {
-  //     it("Should transfer the funds to the owner", async function () {
-  //       const { lock, unlockTime, lockedAmount, owner } = await loadFixture(
-  //         deployOneYearLockFixture
-  //       );
-
-  //       await time.increaseTo(unlockTime);
-
-  //       await expect(lock.withdraw()).to.changeEtherBalances(
-  //         [owner, lock],
-  //         [lockedAmount, -lockedAmount]
-  //       );
-  //     });
-  //   });
-  // });
+      // Additional checks can go here. For example, check if the winner's balance increased.
+    });
+  });
 });
